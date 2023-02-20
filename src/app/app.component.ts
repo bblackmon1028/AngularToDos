d1203d7fa2ed57843f463d108b336b775b662b08
Brittany Blackmon <brittanylynnwright@gmail.com>
2023-02-20 01:06:19 -0500

    Working Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IToDo } from './interfaces/Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularToDos';
+  title = 'To-Do List';
+  toDoList: IToDo[] = [
+    { task: "Clean House", completed: true },
+    { task: "Do Homework", completed: false },
+    { task: "Go Grocery Shopping", completed: true },
+    { task: "Pay Bills", completed: false },
+    { task: "Walk Dog", completed: false }];
+
+  addTask(description: string) {
+    this.toDoList.push({
+      task: description,
+      completed: false
+    });
+  }
+
+  completeTask(todo: IToDo) {
+    let taskToUpdate = this.toDoList.indexOf(todo);
+    todo.completed = true;
+    this.toDoList[taskToUpdate] = todo;
+  };
 }
+
